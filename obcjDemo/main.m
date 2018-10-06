63445c4570b17ef6f63321289e14ecc874580457
wbn <berningwang@icloud.com>
2018-10-07 00:09:26 +0800

    Initial Commit

---
+++ obcjDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  obcjDemo
+//
+//  Created by Berning on 2018/10/7.
+//  Copyright © 2018年 BN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
